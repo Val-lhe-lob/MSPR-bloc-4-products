badf9b728440c953e24ef6682fe3fde90eb235d9
Valentin Lob <valentin.lobraico@gmail.com>
2025-06-11 10:30:21 +0200

    feat(products): added controller and entity

---
+++ MSPR-bloc-4-products/Controllers/ProductController.cs
@@ -0,0 +1,105 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MSPR_bloc_4_products.Data;
+using MSPR_bloc_4_products.Models;
+
+namespace VotreProjet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductDbContext _context;
+
+        public ProductsController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+                return NotFound();
+
+            return product;
+        }
+
+        // GET: api/Products/search?nom=exemple
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<Product>>> SearchProducts(string nom)
+        {
+            if (string.IsNullOrEmpty(nom))
+                return BadRequest("Le paramètre 'nom' est requis.");
+
+            var products = await _context.Products
+                .Where(p => p.Nom.Contains(nom))
+                .ToListAsync();
+
+            return products;
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            product.CreatedAt = DateTime.Now;
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProductById), new { id = product.IdProduit }, product);
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.IdProduit)
+                return BadRequest();
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+                return NotFound();
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.IdProduit == id);
+        }
+    }
+}
---
+++ MSPR-bloc-4-products/Data/ProductDbContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using MSPR_bloc_4_products.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MSPR_bloc_4_products.Data;
+
+public partial class ProductDbContext : DbContext
+{
+    public ProductDbContext()
+    {
+    }
+
+    public ProductDbContext(DbContextOptions<ProductDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-O1NL7RD;Initial Catalog=products_db;Integrated Security=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.IdProduit).HasName("PK__Product__774C53F860EC2AE9");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.IdProduit)
+                .ValueGeneratedNever()
+                .HasColumnName("Id_produit");
+            entity.Property(e => e.Couleur)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("couleur");
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.Nom)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("nom");
+            entity.Property(e => e.Prix)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("prix");
+            entity.Property(e => e.Stock)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("stock");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
